54675e6a9b65cce4ea3616e18e114d67c8ac2c6b
DevRamona <r.ingabire@alustudent.com>
2024-03-01 05:37:49 -0800

    new change

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Module for top_ten function"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
-def top_ten(subreddit):
-    """Query the Reddit API and print the titles of the top 10 posts."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get('data').get('children')
-        for post in data:
-            print(post.get('data').get('title'))
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        print(None)
-
-
-subreddit_name = "learnpython"  
-top_ten(subreddit_name)
+        return 0
